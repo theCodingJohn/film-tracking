e3766134a68b267b556014e6341bffe1714e3368
John Carlo Cunanan <codingjohn@outlook.com>
2021-08-22 17:51:52 +0800

    feat: add recently watched film section

--- pages/profile/dashboard.js
+++ pages/profile/dashboard.js
@@ -14,8 +14,23 @@ const profile = () => {
       </Head>
       <ProfileLayout>
         <section className="w-full">
-          <div className="container max-w-sm mx-auto md:max-w-md md:mx-0 py-4 px-6 md:py-8 md:px-10">
+          <div className="container max-w-sm mx-auto md:max-w-md md:mx-0 py-4 px-6 md:pt-8 md:px-10">
             <H2 className="mb-2" coloredText="Favorite" text="Films" />
+            <div className="flex">
+              {favorite_movies.map((movie) => (
+                <FilmCard
+                  className="mr-1 md:mr-0 w-1/4 md:w-28 md:h-32"
+                  key={movie.id}
+                  imgSrc={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full">
+          <div className="container max-w-sm mx-auto md:max-w-md md:mx-0 py-4 px-6 md:px-10">
+            <H2 className="mb-2" coloredText="Recently" text="Watched" />
             <div className="flex">
               {favorite_movies.map((movie) => (
                 <FilmCard
